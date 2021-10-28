fa26ccefc9404b4a485002413a25a94440f6e059
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-28 13:26:50 -0700

    Update utility scripts.

--- update_repos.sh
+++ update_repos.sh
@@ -3,4 +3,5 @@ git fetch
 git checkout groups-4.x -f
 git fetch
 git reset --hard origin/groups-4.x
+# find . -type d -exec bash -c 'cd "$0" && (`ls .git 2>/dev/null`) || (pwd && git lfs pull)' {} \;
 cd -
