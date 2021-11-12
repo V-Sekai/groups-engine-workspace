56961d373000d7245d2067d735f89ac7c6b3edcb
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-12 12:26:55 -0800

    Update shell script.

--- update_repos.sh
+++ update_repos.sh
@@ -2,5 +2,11 @@ cd godot
 git fetch
 git checkout groups-4.x -f
 git fetch
+git reset --hard origin/groups-4.xcd godot
+cd -
+cd godot-launcher
+git fetch
+git checkout groups-4.x -f
+git fetch
 git reset --hard origin/groups-4.x
 cd -
