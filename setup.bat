add5c10971e404384bc0605adc5c7000026a24d2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-13 20:29:39 -0800

    Update scripts.

--- setup.bat
+++
@@ -1,5 +0,0 @@
-# Can't use pushInstead because of git lfs
-git config --global url.git@github.com:fire.pushInsteadOf https://github.com/fire
-git config --global url.git@github.com:godot-extended-libraries.pushInsteadOf https://github.com/godot-extended-libraries
-git config --global url.git@gitlab.com:.insteadOf https://gitlab.com/
-git config --global url.git@gitlab.com:SaracenOne.pushInsteadOf https://gitlab.com/SaracenOne
--- update_repos.sh
+++ update_repos.sh
@@ -4,9 +4,3 @@ git checkout groups-4.x -f
 git fetch
 git reset --hard origin/groups-4.x
 cd -
-cd godot-launcher
-git fetch
-git checkout groups-web-4.x -f
-git fetch
-git reset --hard origin/groups-web-4.x
-cd -
