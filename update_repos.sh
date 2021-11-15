d33997eef15abde5f802d468fd307153e38abbcf
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-14 18:18:31 -0800

    Remove launcher.

--- update_repos.sh
+++ update_repos.sh
@@ -3,10 +3,3 @@ git fetch
 git checkout groups-4.x -f
 git fetch
 git reset --hard origin/groups-4.x
-cd -
-cd godot-launcher
-git fetch
-git checkout groups-web-4.x -f
-git fetch
-git reset --hard origin/groups-web-4.x
-cd -
